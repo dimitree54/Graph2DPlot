19119ee140968b3ad13472b67153d1a184cec403
Dmitrii-Mac <d.rashchenko@planner5d.com>
2021-07-21 21:39:05 +0300

    background mode

--- client/src/main/kotlin/we/rashchenko/main.kt
+++ client/src/main/kotlin/we/rashchenko/main.kt
@@ -1,21 +1,21 @@
 package we.rashchenko
 
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PointMode
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import we.rashchenko.environments.Environment
 import we.rashchenko.environments.SimpleEnvironment
-import we.rashchenko.networks.*
+import we.rashchenko.networks.NeuralNetworkIn2DBuilder
+import we.rashchenko.networks.NeuralNetworkIn2DSpace
+import we.rashchenko.networks.StochasticNeuralNetwork
+import we.rashchenko.networks.getActiveAndPassiveCoordinates
 import we.rashchenko.neurons.BinaryNeuron
 import we.rashchenko.neurons.StochasticBinaryNeuron
 import we.rashchenko.utils.Vector2
@@ -29,40 +29,16 @@ val colorPassive = Color.Red
 val backColor = Color.Black
 val connectionColor = Color.Gray
 
-fun DrawScope.drawNN(nn2d: NeuralNetworkIn2DSpace){
-	drawRect(backColor)
-	val scale = Vector2(size.width, size.height)
-	nn2d.getConnections(scale).forEach { (source, receiver) ->
-		drawLine(connectionColor, Offset(source.x, source.y), Offset(receiver.x, receiver.y)) }
-	drawPoints(nn2d.getExternalCoordinates(scale).map { Offset(it.x, it.y) },
-		PointMode.Points, externalColor, strokeWidth = 15f, cap = StrokeCap.Round)
-}
-
-fun DrawScope.drawNNState(activePositions: Collection<Vector2>, passivePositions: Collection<Vector2>){
-	drawPoints(activePositions.map { Offset(it.x, it.y) }, PointMode.Points, colorActive,
-		strokeWidth = 5f, cap = StrokeCap.Round)
-	drawPoints(passivePositions.map { Offset(it.x, it.y) }, PointMode.Points, colorPassive,
-		strokeWidth = 5f, cap = StrokeCap.Round)
-}
-
-@Composable
-fun networkStateDrawer(activeAndPassiveNeurons: MutableState<Pair<List<Vector2>, List<Vector2>>>, modifier: Modifier){
-	Canvas(modifier = modifier){
-		val scale = Vector2(size.width, size.height)
-		drawNNState(activeAndPassiveNeurons.value.first.map { it.scl(scale) },
-			activeAndPassiveNeurons.value.second.map { it.scl(scale) })
-	}
-}
 
 val random = Random()
-fun rectangularPositionSampler(rectangle: Vector2 = Vector2(2560f, 1600f)): Vector2{
+fun rectangularPositionSampler(rectangle: Vector2 = Vector2(2560f, 1600f)): Vector2 {
 	val maxDim = max(rectangle.x, rectangle.y)
 	var x: Float
 	var y: Float
-	while (true){
+	while (true) {
 		x = random.nextFloat()
 		y = random.nextFloat()
-		if (x < rectangle.x / maxDim && y < rectangle.y / maxDim){
+		if (x < rectangle.x / maxDim && y < rectangle.y / maxDim) {
 			break
 		}
 	}
@@ -70,14 +46,17 @@ fun rectangularPositionSampler(rectangle: Vector2 = Vector2(2560f, 1600f)): Vect
 	return Vector2(x * maxDim / rectangle.x, y * maxDim / rectangle.y)
 }
 
-fun main(){
+@ObsoleteCoroutinesApi
+fun main() {
 	val environment = SimpleEnvironment()
 	val nn = NeuralNetworkIn2DSpace(StochasticNeuralNetwork())
-	object: NeuralNetworkIn2DBuilder(nn){
+	object : NeuralNetworkIn2DBuilder(nn) {
 		override fun neuronsWithPositionSampler(): Pair<BinaryNeuron, Vector2> =
 			StochasticBinaryNeuron() to rectangularPositionSampler()
+
 		override fun environmentInputsPositionSampler(environment: Environment): Collection<Vector2> =
 			environment.externalSignals.map { rectangularPositionSampler() }
+
 		override fun connectionsSampler(allPositions: Collection<Vector2>): Map<Vector2, Collection<Vector2>> =
 			kNearest(5, allPositions)
 	}.apply {
@@ -86,21 +65,52 @@ fun main(){
 		addConnections()
 	}
 
-	runBlocking {
-		Window {
-			val v = remember { mutableStateOf(Pair(emptyList<Vector2>(), emptyList<Vector2>())) }
-			Canvas(modifier = Modifier.fillMaxSize()){
-				drawNN(nn)
+	Window {
+		val ticksPerSec = remember { mutableStateOf(0L) }
+		val nnRunning = remember { mutableStateOf(false) }
+		val visualMode = remember { mutableStateOf(false) }
+		val nnState = remember { mutableStateOf(Pair(emptyList<Vector2>(), emptyList<Vector2>())) }
+		val coroutineScope = rememberCoroutineScope()
+		coroutineScope.launch {
+			var prevStep: Long
+			while (true) {
+				prevStep = nn.timeStep
+				delay(1000)
+				ticksPerSec.value = nn.timeStep - prevStep
 			}
-			networkStateDrawer(v, modifier = Modifier.fillMaxSize())
-
-			val composableScope = rememberCoroutineScope()
-			composableScope.launch {
-				while (true) {
-					nn.tick()
-					v.value = nn.getActiveAndPassiveCoordinates()
-					delay(100)
+		}
+		if (visualMode.value) {
+			networkDrawer(nn, nnState, onClick = { visualMode.value = false })
+			Text("TPS: ${ticksPerSec.value.toInt()}", color = Color.Yellow)
+		} else {
+			Column {
+				Text("TPS: ${ticksPerSec.value.toInt()}")
+				Button(onClick = { onRunClick(nnRunning, coroutineScope, nn, visualMode, nnState) }) {
+					Text(if (nnRunning.value) "Pause NN" else "Run NN")
 				}
+				Button(onClick = { visualMode.value = true }) {
+					Text("Show NN")
+				}
+			}
+		}
+	}
+}
+
+@ObsoleteCoroutinesApi
+fun onRunClick(
+	nnRunning: MutableState<Boolean>,
+	coroutineScope: CoroutineScope,
+	nn: NeuralNetworkIn2DSpace,
+	visualMode: MutableState<Boolean>,
+	nnState: MutableState<Pair<List<Vector2>, List<Vector2>>>
+) {
+	nnRunning.value = !nnRunning.value
+	coroutineScope.launch(context = newSingleThreadContext("NNThread")) {
+		while (nnRunning.value) {
+			nn.tick()
+			if (visualMode.value) {
+				nnState.value = nn.getActiveAndPassiveCoordinates()
+				delay(100)
 			}
 		}
 	}
