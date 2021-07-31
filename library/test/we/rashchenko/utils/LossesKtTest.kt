4e0fce35bb3c7f35d61f7836933c03abbb90e608
Dmitrii-Mac <d.rashchenko@planner5d.com>
2021-07-31 08:30:58 +0300

    hemming distance test

---
+++ library/test/we/rashchenko/utils/LossesKtTest.kt
@@ -0,0 +1,35 @@
+package we.rashchenko.utils
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class LossesKtTest {
+	@Test
+	fun testHemmingDistance() {
+		assertEquals(
+			hemmingDistance(
+				listOf(false, false, false, false),
+				listOf(false, false, false, false)),
+			0.0
+		)
+		assertEquals(
+			hemmingDistance(
+				listOf(true, true, true, true),
+				listOf(false, false, false, false)),
+			4.0
+		)
+		assertEquals(
+			hemmingDistance(
+				listOf(true, true, true, true),
+				listOf(true, true, true, true)),
+			0.0
+		)
+		assertEquals(
+			hemmingDistance(
+				listOf(false, true, true, true),
+				listOf(true, true, true, false)),
+			2.0
+		)
+	}
+}
